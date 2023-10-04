cab3d0eae8af5cf6862aa05f699aca2081a76648
namkuner <namkuner@gmail.com>
2023-10-05 05:46:53 +0700

    push weight

--- asian_dataset_processing.py
+++ asian_dataset_processing.py
@@ -4,27 +4,27 @@ from align_trans import Face_alignment
 import torch
 from torchvision import transforms as trans
 # Đường dẫn đến thư mục mẹ chứa tất cả các thư mục con
-parent_directory = '/path/to/parent_directory'
+parent_directory = 'D:\PY_ML\Deep_Learning_1\AI_Algorithm'
 from MTCNN import create_mtcnn_net
 # Đường dẫn đến thư mục lưu kết quả sau khi alignment
-output_directory = '/path/to/output_directory'
+op = 'D:\output'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 test_transform = trans.Compose([
     trans.ToTensor(),
     trans.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
-def detec_and_align():
+def detec_and_align(img):
     try:
-        img = cv2.imread(image_path)
-
         if img.shape != None:
             bboxes, landmarks = create_mtcnn_net(img, 20, device,
                                                  p_model_path='Weights/pnet_Weights',
                                                  r_model_path='Weights/rnet_Weights',
                                                  o_model_path='Weights/onet_Weights')
+            img = Face_alignment(img, default_square=True, landmarks=landmarks)
+
+            return img[0]
     except Exception as e:
         print(e)
-        img = Face_alignment(img, default_square=True, landmarks=landmarks)
-        return img[0]
+
 for root, dirs, files in os.walk(parent_directory):
     for file in files:
         if file.lower().endswith(('.png', '.jpg', '.jpeg')):
@@ -36,7 +36,9 @@ for root, dirs, files in os.walk(parent_directory):
                 person_name = os.path.basename(os.path.dirname(image_path))
 
                 # Tạo thư mục con trong thư mục output
-                output_directory = os.path.join(output_directory, person_name)
+                output_directory =""
+                output_directory = os.path.join(op, person_name)
+                print(output_directory)
                 if not os.path.exists(output_directory):
                     os.makedirs(output_directory)
 
